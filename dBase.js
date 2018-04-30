578672d92b1819c3993050df8a0dadc3d49a4d6f
Mollert <blueskies@carolina.rr.com>
2018-04-30 16:51:55 -0400

    added reply and media for first page

--- dBase.js
+++ dBase.js
@@ -102,20 +102,30 @@ const cannedGoods = [
 const comments = [
 		{
 			"id": 0,
+			"replyId": "",
 			"date": "1:01:2018",
 			"name": "Patty",
 			"comment": "What a wonderful bounty"
 		},
 		{
 			"id": 1,
+			"replyId": "",
 			"date": "2:10:2018",
 			"name": "Henry",
 			"comment": "Just found you . . . I'll be back"
 		},
-				{
+		{
 			"id": 2,
+			"replyId": "",
 			"date": "4:18:2018",
 			"name": "Roberta",
 			"comment": "I am looking for some organically grown brussel sprouts.  Do you have a source?"
+		},
+		{
+			"id": 3,
+			"replyId": "1",
+			"date": "5:1:2018",
+			"name": "Ginny",
+			"comment": "We'll look for you then.  Every Saturday."
 		}
 	];
--- javascriptBlog.js
+++ javascriptBlog.js
@@ -1,13 +1,39 @@
 
+const getInputs = () => {
+	let name = $("#byWho").val().trim();
+	let comment = $("#addedWhat").val().trim();
+	let temp = new Date();
+	let date = `${(temp.getMonth() + 1)}:${temp.getDate()}:${temp.getFullYear()}`;
+	
+	$("#byWho").val("");
+	$("#addedWhat").val("");
+	$("#overallList").empty();	
+
+	return [name, comment, date];
+};
+
 const getComments = () => {
 	comments.forEach(function(item) {
-		let liTemp = $('<li/>').attr('class', 'entry');
-		let nameDiv = $('<div/>').attr('class', 'name').text(`Entry by: ${item.name}`);
-		$(nameDiv).append($("<span class='date'></span>").text(item.date));
-		let postDiv = $('<div/>').attr('class', 'post').text(item.comment);
-		$(liTemp).append(nameDiv).append(postDiv);		
-
-		$('#overallList').append(liTemp);
+		 if (item.replyId) {
+			let liTemp = $('<li/>').attr('class', 'reply').attr('id', item.id);
+			let nameDiv = $('<div/>').attr('class', 'name').text(`Reply by: ${item.name}`);
+			$(nameDiv).append($("<span class='date'></span>").text(item.date));
+			let postDiv = $('<div/>').attr('class', 'post').text(item.comment);
+			$(liTemp).append(nameDiv).append(postDiv);
+			let replyInput = $('<input type="button" value="Add a Reply"/>').attr('class', 'replyBtn');	
+			$(liTemp).append(nameDiv).append(postDiv).append(replyInput);
+			
+			$(`#${item.replyId}`).append(liTemp);
+		} else {		
+			let liTemp = $('<li/>').attr('class', 'entry').attr('id', item.id);
+			let nameDiv = $('<div/>').attr('class', 'name').text(`Entry by: ${item.name}`);
+			$(nameDiv).append($("<span class='date'></span>").text(item.date));
+			let postDiv = $('<div/>').attr('class', 'post').text(item.comment);
+			let replyInput = $('<input type="button" value="Add a Reply"/>').attr('class', 'replyBtn');	
+			$(liTemp).append(nameDiv).append(postDiv).append(replyInput);
+		
+			$('#overallList').append(liTemp);
+		}
 	});
 };
 
@@ -16,16 +42,14 @@ $(window).on("load", function() {
 });
 
 $("#button").on("click", function() {	
-	let name = $("#byWho").val().trim();
-	let comment = $("#addedWhat").val().trim();
-	let temp = new Date();
-	let date = `${(temp.getMonth() + 1)}:${temp.getDate()}:${temp.getFullYear()}`;
-	
-	comments.push({"id": comments.length, "date": date, "name": name, "comment": comment});
+	let theInputs = getInputs();
+	comments.push({"id": comments.length,  "replyId": "", "date": theInputs[2], "name": theInputs[0], "comment": theInputs[1]});
+	getComments();
+});
 
-	$("#byWho").val("");
-	$("#addedWhat").val("");
-	$("#overallList").empty();
-
+$("#overallList").on("click", ".replyBtn", function() {
+	let parentId = $(this).parent().attr("id");	
+	let theInputs = getInputs();	
+	comments.push({"id": comments.length, "replyId": parentId, "date": theInputs[2], "name": theInputs[0], "comment": theInputs[1]});
 	getComments();
 });
